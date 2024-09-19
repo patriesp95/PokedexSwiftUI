e60e8551df318c74954c17acd574a2c17ddf74bf
Patricia Martinez Espert <patrimespert@outlook.com>
2024-09-19 18:24:17 +0200

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by Patricia Martínez Espert on 19/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokedexSwiftUITests/PokedexSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUITests.swift
+//  PokedexSwiftUITests
+//
+//  Created by Patricia Martínez Espert on 19/9/24.
+//
+
+import Testing
+@testable import PokedexSwiftUI
+
+struct PokedexSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
