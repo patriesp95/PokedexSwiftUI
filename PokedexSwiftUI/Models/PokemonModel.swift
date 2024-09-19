9fb7d960dec3b94053d2cbf1a24dc9242867922d
Patricia Martinez Espert <patrimespert@outlook.com>
2024-09-19 23:08:24 +0200

    Helpers and PokemonModel done

---
+++ PokedexSwiftUI/Models/PokemonModel.swift
@@ -0,0 +1,28 @@
+//
+//  PokemonModel.swift
+//  PokedexSwiftUI
+//
+//  Created by Patricia Martínez Espert on 19/9/24.
+//
+
+import Foundation
+
+struct PokemonPage: Codable {
+    let count: Int
+    let next: String
+    let results: [Pokemon]
+}
+
+struct Pokemon: Codable, Identifiable, Equatable {
+    let id = UUID()
+    let name: String
+    let url: String
+    
+    static var samplePokemon = Pokemon(name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/")
+}
+
+struct DetailPokemon: Codable {
+    let id: Int
+    let height: Int
+    let weight: Int
+}
